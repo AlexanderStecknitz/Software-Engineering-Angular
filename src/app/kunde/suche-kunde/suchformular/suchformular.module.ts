217cd19ad38ad9d926a2d9c6c308ace95408fd30
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-06-07 21:59:40 +0200

    feat: SuchformularModule

--- src/app/kunde/suche-kunde/suchformular/suchformular.module.ts
+++ src/app/kunde/suche-kunde/suchformular/suchformular.module.ts
@@ -7,14 +7,22 @@ import { FormsModule } from '@angular/forms';
 import { SuchformularComponent } from './suchformular.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
-import { SucheNachnameModule } from './nachname/suche-nachname.module';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { SucheFamilienstandModule } from './suche-familienstand.module';
+import { SucheGeschlechtTypeModule } from './suche-geschlechttype.module';
+import { SucheNachnameModule } from './suche-nachname.module';
+import { SucheInteressenModule } from './suche-interessen.module';
 @NgModule({
     declarations: [SuchformularComponent],
     exports: [SuchformularComponent],
     imports: [
         FormsModule,
         HttpClientModule,
+        MatGridListModule,
         MatIconModule,
+        SucheFamilienstandModule,
+        SucheGeschlechtTypeModule,
+        SucheInteressenModule,
         SucheNachnameModule,
     ],
 })
