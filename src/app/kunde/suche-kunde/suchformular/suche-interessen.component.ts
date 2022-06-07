b3ddfa9757327c72fde88d61b4cdf7d3ad2cd8b6
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-06-07 21:59:00 +0200

    InteressenComponent

---
+++ src/app/kunde/suche-kunde/suchformular/suche-interessen.component.ts
@@ -0,0 +1,50 @@
+import { Component, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import log from 'loglevel';
+
+@Component({
+    selector: 'hs-suche-interessen',
+    templateUrl: './suche-interessen.component.html',
+})
+export class SucheInteressenComponent {
+    lesen = false;
+
+    sport = false;
+
+    reisen = false;
+
+    @Output()
+    readonly lesen$ = new Subject<boolean>();
+
+    @Output()
+    readonly sport$ = new Subject<boolean>();
+
+    @Output()
+    readonly reisen$ = new Subject<boolean>();
+
+    constructor() {
+        log.debug('SucheInteressenComponent.constructor()');
+    }
+
+    onChangeLesen(event: Event) {
+        // https://stackoverflow.com/questions/44321326/property-value-does-not-exist-on-type-eventtarget-in-reisen
+        const { checked } = event.target as HTMLInputElement;
+        log.debug(`SucheInteressenComponent.onChangeLesen: checked=${checked}`);
+        this.lesen$.next(checked);
+    }
+
+    onChangeSport(event: Event) {
+        // https://stackoverflow.com/questions/44321326/property-value-does-not-exist-on-type-eventtarget-in-reisen
+        const { checked } = event.target as HTMLInputElement;
+        log.debug(`SucheInteressenComponent.onChangeSport: checked=${checked}`);
+        this.sport$.next(checked);
+    }
+
+    onChangeReisen(event: Event) {
+        const { checked } = event.target as HTMLInputElement;
+        log.debug(
+            `SucheInteressenComponent.onChangeReisen: checked=${checked}`,
+        );
+        this.reisen$.next(checked);
+    }
+}
