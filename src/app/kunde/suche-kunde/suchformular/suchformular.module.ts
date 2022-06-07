b7a1d61a7cd58f0efafa4043f3678e2b189c9085
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-06-07 22:49:07 +0200

    feat: Suchformular Design

--- src/app/kunde/suche-kunde/suchformular/suchformular.module.ts
+++ src/app/kunde/suche-kunde/suchformular/suchformular.module.ts
@@ -6,6 +6,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SuchformularComponent } from './suchformular.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { SucheFamilienstandModule } from './suche-familienstand.module';
@@ -18,8 +20,10 @@ import { SucheInteressenModule } from './suche-interessen.module';
     imports: [
         FormsModule,
         HttpClientModule,
+        MatButtonModule,
         MatGridListModule,
         MatIconModule,
+        MatTooltipModule,
         SucheFamilienstandModule,
         SucheGeschlechtTypeModule,
         SucheInteressenModule,
