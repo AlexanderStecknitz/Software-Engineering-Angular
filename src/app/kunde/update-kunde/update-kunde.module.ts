56256524aed0456183edf5b133e5291cb437ca56
alexseitz <alexseitz248@gmail.com>
2022-06-17 17:46:37 +0200

    feat: changes to update-kunde

--- src/app/kunde/update-kunde/update-kunde.module.ts
+++ src/app/kunde/update-kunde/update-kunde.module.ts
@@ -18,18 +18,25 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { Title } from '@angular/platform-browser';
+import { UpdateEmailComponent } from './update-email.component';
 import { UpdateFamilienstandComponent } from './update-familienstand.component';
 import { UpdateGeschlechtComponent } from './update-geschlecht.component';
-import { UpdateInteressenModule } from './update-interessen.module';
-// import { UpdateKundeComponent } from './update-kunde.component';
+import { UpdateInteressenComponent } from './update-interessen.component';
+import { UpdateKategorieComponent } from './update-kategorie.component';
+import { UpdateKundeComponent } from './update-kunde.component';
+import { UpdateNachnameComponent } from './update-nachname.component';
 
 @NgModule({
-    imports: [SharedModule, UpdateInteressenModule],
+    imports: [SharedModule],
     declarations: [
+        UpdateEmailComponent,
         UpdateFamilienstandComponent,
         UpdateGeschlechtComponent,
-        // UpdateKundeComponent,
+        UpdateInteressenComponent,
+        UpdateKategorieComponent,
+        UpdateKundeComponent,
+        UpdateNachnameComponent,
     ],
     providers: [Title],
 })
-export class UpdateKundeModule {}
+export class UpdateBuchModule {}
