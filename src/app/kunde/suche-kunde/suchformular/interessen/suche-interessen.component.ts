a0da1f9421821dc2c2e8c8b9a66aa141749c11cb
Alexander Stecknitz <Alex.Stecknitz@web.de>
2022-06-17 18:21:25 +0200

    fix: interessen queryParams

--- src/app/kunde/suche-kunde/suchformular/interessen/suche-interessen.component.ts
+++ src/app/kunde/suche-kunde/suchformular/interessen/suche-interessen.component.ts
@@ -1,4 +1,5 @@
 import { Component, Output } from '@angular/core';
+import type { MatCheckboxChange } from '@angular/material/checkbox';
 import { Subject } from 'rxjs';
 import log from 'loglevel';
 
@@ -27,25 +28,26 @@ export class SucheInteressenComponent {
         log.debug('SucheInteressenComponent.constructor()');
     }
 
-    onChangeLesen(event: Event) {
+    onChangeLesen(event: MatCheckboxChange) {
         // https://stackoverflow.com/questions/44321326/property-value-does-not-exist-on-type-eventtarget-in-reisen
-        const { checked } = event.target as HTMLInputElement;
-        log.debug(`SucheInteressenComponent.onChangeLesen: checked=${checked}`);
-        this.lesen$.next(checked);
-    }
-
-    onChangeSport(event: Event) {
-        // https://stackoverflow.com/questions/44321326/property-value-does-not-exist-on-type-eventtarget-in-reisen
-        const { checked } = event.target as HTMLInputElement;
-        log.debug(`SucheInteressenComponent.onChangeSport: checked=${checked}`);
-        this.sport$.next(checked);
-    }
-
-    onChangeReisen(event: Event) {
-        const { checked } = event.target as HTMLInputElement;
         log.debug(
-            `SucheInteressenComponent.onChangeReisen: checked=${checked}`,
+            `SucheInteressenComponent.onChangeLesen: checked=${event.checked}`,
         );
-        this.reisen$.next(checked);
+        this.lesen$.next(event.checked);
+    }
+
+    onChangeSport(event: MatCheckboxChange) {
+        // https://stackoverflow.com/questions/44321326/property-value-does-not-exist-on-type-eventtarget-in-reisen
+        log.debug(
+            `SucheInteressenComponent.onChangeSport: checked=${event.checked}`,
+        );
+        this.sport$.next(event.checked);
+    }
+
+    onChangeReisen(event: MatCheckboxChange) {
+        log.debug(
+            `SucheInteressenComponent.onChangeReisen: checked=${event.checked}`,
+        );
+        this.reisen$.next(event.checked);
     }
 }
